76a405d08290a3b1e98c0422157e1591d19739b0
unknown <csumagang@csumb.edu>
2020-07-29 00:37:38 -0700

    fixed bugs

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -15,7 +15,7 @@ const Nav = () => {
                 <div className="uk-navbar-left">
                   <ul className="uk-navbar-nav">
                     <li>
-                      <Link to="/">Strapi Blog</Link>
+                      <Link to="/">Xinampa Blog</Link>
                     </li>
                   </ul>
                 </div>
--- frontend/src/utils/apolloClient.js
+++ frontend/src/utils/apolloClient.js
@@ -4,7 +4,7 @@ import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
+  uri: `http://localhost:1337/graphql`
 });
 const client = new ApolloClient({
   cache,
